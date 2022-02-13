eb6b7cddaaf72330cdbca8d8c725e292ee3edf44
khushi verma <73602440+khushi-kv@users.noreply.github.com>
2022-02-13 21:17:35 +0530

    Update index.js

--- TodoList Using React State/index.js
+++ TodoList Using React State/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReacDOM from 'react-dom';
 
-//import ComA from "./ComA";
-//import IncDec from "./incrementing";
-//import "./component/TabMenu/PhotoGallery";
+
 import App from "./App";
 import "./component/TabMenu/Todo.css";
 ReacDOM.render(<App/>,document.getElementById('root'))
