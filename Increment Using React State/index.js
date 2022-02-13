3e27d7b523773a37009c2e355bda049e2cf32d28
khushi verma <73602440+khushi-kv@users.noreply.github.com>
2022-02-13 21:18:11 +0530

    Update index.js

--- Increment Using React State/index.js
+++ Increment Using React State/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReacDOM from 'react-dom';
 
-//import ComA from "./ComA";
+
 import IncDec from "./incrementing";
 import "./component/TabMenu/PhotoGallery";
 ReacDOM.render(<IncDec/>,document.getElementById('root'))
